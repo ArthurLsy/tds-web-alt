2d2f6378fb1f7bbbac1c39847e543b70dfe210cb
Arthur LAISNEY <alaisney7@gmail.com>
2022-11-22 22:18:43 +0100

    newpage

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <title>Projets</title>
+</head>
+<body>
+<div id="menu">
+    <div class="btn_div"><a href="" class="btn_menu">Projets AP</a><span class="material-symbols-outlined btn_croix">close</span></div>
+    <div class="btn_div"><a href="" class="btn_menu">TD SLAM</a><span class="material-symbols-outlined btn_croix">close</span></div>
+    <div class="btn_div"><a href="" class="btn_menu">Projets perso</a><span class="material-symbols-outlined btn_croix">close</span></div>
+</div>
+</body>
+</html>
