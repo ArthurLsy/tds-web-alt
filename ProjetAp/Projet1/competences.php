7633ddcf3f1dd2547a5eb29a1326091119143654
Arthur LAISNEY <alaisney7@gmail.com>
2022-12-03 22:55:22 +0100

    Menu

--- ProjetAp/Projet1/competences.php
+++ ProjetAp/Projet1/competences.php
@@ -1,7 +1,9 @@
+<?php include 'utils/header.php'; ?>
+
 <div id="competences">
     <div id="box_competences">
         <?php
-        $data = yaml_parse_file("ressources/yaml/competences.yaml");
+        $data = yaml_parse_file("../ressources/yaml/competences.yaml");
         foreach ($data as $domaine => $tbCompetences) {
             echo '<h1>' . $domaine . '</h1>';
             foreach ($tbCompetences as $competence) :?>
@@ -11,6 +13,7 @@
                         <div><?php echo $competence['nom'] ?> :</div>
                     </div>
                     <div class="box_progression">
+                        <div id="pourcent"><?php echo $competence['niveau'] ?> %</div>
                         <div class="barre_progression">
                             <div class="progression" style="width: <?php echo $competence['niveau'] ?>%"></div>
                         </div>
@@ -24,7 +27,7 @@
         <?php
             foreach ($data as $domaine => $tbCompetences) {
                 foreach ($tbCompetences as $competence) :?>
-                    <div class="box_detail" style="transform: scale(0);">
+                    <div class="box_detail" "id=box_<?php echo $competence["nom"] ?>" style="font-size: 0;">
                         <h1><?php echo $competence['nom'] ?> </h1>
                         <div><?php echo $competence['description'] ?> </div>
                     </div>
@@ -34,5 +37,8 @@
     </div>
 </div>
 <script>
-
+let items = document.getElementsByClassName('input')
+items[0].show()
 </script>
+
+<?php include 'utils/footer.php'; ?>
--- ProjetAp/Projet1/utils/navbar.php
+++ ProjetAp/Projet1/utils/navbar.php
@@ -1,8 +1,9 @@
 <header id="header" class="navbar">
-    <div><a href="" class="menu">Accueil</a></div>
+    <div><a href="" class="menu"><a href="index.php">Accueil</a></a></div>
     <div><a href="" class="menu">A propos</a></div>
-    <div><a href="" class="menu">Comtpétences</a></div>
+    <div><a href="" class="menu"><a href='competences.php'>Comtpétences</a></a></div>
     <div><a href="" class="menu">Experiences</a></div>
     <div><a href="" class="menu">Formations</a></div>
+    <div><a href="" class="menu">Projets</a></div>
     <div><a href="" class="contact">Contact</a></div>
 </header>
