0feff0848f87bb61db49659363af916b514a6251
Arthur LAISNEY <alaisney7@gmail.com>
2022-12-13 16:50:50 +0100

    petits trucs

--- ProjetAp/portfolio/utils/navbar.php
+++ ProjetAp/portfolio/utils/navbar.php
@@ -1,7 +1,7 @@
 <header id="header" class="navbar">
     <div><a href="index.php" class="menu">Accueil</a></div>
     <div><a href="about.php" class="menu">A propos</a></div>
-    <div><a href="competences.php" class="menu">Comtpétences</a></div>
+    <div><a href="competences.php" class="menu">Compétences</a></div>
     <div><a href="experience.php" class="menu">Experiences</a></div>
     <div><a href="formations.php" class="menu">Formations</a></div>
     <div><a href="contact.php" class="contact">Contact</a></div>
